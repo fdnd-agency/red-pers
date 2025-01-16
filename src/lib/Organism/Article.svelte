057598995234dd3c7885d99b258ddc4b05aae2d7
RubenErhardt <rubengamebattles@outlook.com>
2025-01-16 14:33:16 +0100

    first article
    -responsive
    -bug fixes

--- src/lib/Organism/Article.svelte
+++ src/lib/Organism/Article.svelte
@@ -7,53 +7,50 @@
         day: 'numeric',
     };
 </script>
-
-
 <a href="/{post.slug}">
     <article class="article {isFirst ? 'first-article' : 'other-articles'}" 
         style={isFirst ? 'max-width:1240px' : 'max-width: 25em;'}>
-
-        <!-- Voeg hier de badge toe voor het nieuwste artikel -->
+        
         {#if isFirst}
         <div class="newest">NIEUWSTE ARTIKEL</div>
         {/if}
         
         <img loading={isFirst ? "eager" : "lazy"} src={post.yoast_head_json.og_image[0].url} alt="Artikel afbeelding" width="25em" height="15em">
-
         
         <div class="everything">
-        <div class="title-info-text">
-            <div class="title">
-                <h1 class="artikel-kop">{@html post.title.rendered}</h1>
-            </div>
-            
-            <!-- Excerpt alleen zichtbaar als het eerste artikel is -->
-            {#if isFirst}
-                <p class="excerpt">{@html post.excerpt.rendered}</p>
-            {/if}
+            <div class="title-info-text">
+                {#if isFirst}
+                    <h1 class="artikel-kop">{@html post.title.rendered}</h1>
+                {:else}
+                    <div class="title">
+                        <h1 class="artikel-kop">{@html post.title.rendered}</h1>
+                    </div>
+                {/if}
+                
+                {#if isFirst}
+                    <p class="excerpt">{@html post.excerpt.rendered}</p>
+                {/if}
 
-            <div class="info-text">
-                <p>
-                    <span class="author">{post.yoast_head_json.author}</span>
-                    <span class="reading-time">
-                        <span aria-hidden="true">&minus;</span>
-                        <abbr title="Geschatte leestijd">
-                            {post.yoast_head_json.twitter_misc["Geschatte leestijd"]?.replace("minuten", "")}
-                            <span aria-label="minuten">min</span>
-                        </abbr>
-                    </span>
-                </p>
-                <p>{(new Date(post.date)).toLocaleDateString("nl-NL", dateFormat)}</p>
-            </div>
+                <div class="info-text">
+                    <p>
+                        <span class="author">{post.yoast_head_json.author}</span>
+                        <span class="reading-time">
+                            <span aria-hidden="true">&minus;</span>
+                            <abbr title="Geschatte leestijd">
+                                {post.yoast_head_json.twitter_misc["Geschatte leestijd"]?.replace("minuten", "")}
+                                <span aria-label="minuten">min</span>
+                            </abbr>
+                        </span>
+                    </p>
+                    <p>{(new Date(post.date)).toLocaleDateString("nl-NL", dateFormat)}</p>
+                </div>
 
+            </div>
         </div>
-    </div>
     </article>
 </a>
 
-
 <style>
-
     img {
         width: 100%;
         height: 15em;
@@ -75,21 +72,16 @@
         overflow: hidden;
     }
 
-
     .first-article .everything{
-        padding: 2em;
-        width: 50%;
+        padding: 1em;
     }
 
     .first-article .excerpt {
-    font-size: 16px; /* Pas eventueel de lettergrootte aan voor de excerpt */
-    margin-top: -5em;
-    line-height: 1.5;
-}
-
+        font-size: 16px;
+        line-height: 1.5;
+    }
 
     .info-text {
-
         display: flex;
         justify-content: space-between;
         align-items: center;
@@ -103,46 +95,46 @@
         font-weight: bold;
     }
 
-@media only screen and (width < 35em) {
-    article {
-        max-width: unset;
-        display: flex;
+    @media only screen and (width < 35em) {
+        article {
+            max-width: unset;
+            display: flex;
+        }
+
+        img {
+            width: 42%;
+            height: 10em;
+            object-fit: cover;
+            border-bottom: none;
+        }
+
+        .title-info-text {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .title {
+            height: 100%;
+        }
+
+        h1 {
+            font-size: calc(var(--title-font-size) * 2 / 3);
+            line-height: calc(var(--title-font-line-height) * 2 / 3);
+            margin: 0;
+        }
+
+        .info-text {
+            border-top: none;
+            padding: 0 0.5em;
+            font-size: 12px;
+        }
+
+        .reading-time {
+            display: none;
+        }
     }
 
-    img {
-        width: 42%;
-        height: 10em;
-        object-fit: cover;
-        border-bottom: none;
-    }
-
-    .title-info-text {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .title {
-        height: 100%;
-    }
-
-    h1 {
-        font-size: calc(var(--title-font-size) * 2 / 3); /* make font 33% smaller */
-        line-height: calc(var(--title-font-line-height) * 2 / 3);
-        margin: 0;
-    }
-
-    .info-text {
-        border-top: none;
-        padding: 0 0.5em;
-        font-size: 12px;
-    }
-
-    .reading-time {
-        display: none;
-    }
-}
-
-.other-articles {
+    .other-articles {
         --title-font-size: 28px;
         --title-font-line-height: 32px;
         max-width: 25em;
@@ -150,38 +142,36 @@
         background-color: var(--background-color);
     }
 
-
     .first-article {
         display: flex;
-        margin-top: 20px;
         position: relative;
-        flex-direction: row;  /* Zorgt ervoor dat afbeelding en tekst naast elkaar staan */
-        border-left: 4px solid #D5302D; /* Rode lijn aan de linkerkant voor accent */
+        flex-direction: row;
+        border-left: 4px solid #D5302D;
         background-color: var(--background-color);
         max-width: 1240px;
-        margin-bottom: 1.5em;
+        z-index: 0;
         box-sizing: border-box;
-        transition: transform 0.3s ease, box-shadow 0.3s ease; /* Zachte overgang voor de hover-effecten */
+        transition: transform 0.3s ease, box-shadow 0.3s ease;
     }
 
     .first-article .newest {
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    background-color: rgba(255, 0, 0, 0.7); /* Rood met doorzichtige achtergrond */
-    color: white;
-    font-size: 14px;
-    padding: 5px 10px;
-    font-weight: bold;
-    text-transform: uppercase;
-    border-radius: 3px;
-}
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        background-color: rgba(255, 0, 0, 0.7);
+        color: white;
+        font-size: 14px;
+        padding: 5px 10px;
+        font-weight: bold;
+        text-transform: uppercase;
+        border-radius: 3px;
+    }
 
     .first-article img {
         width: 100%;
         height: 26em;
         border-bottom: var(--border);
-        object-fit: cover; /* Zorgt ervoor dat de afbeelding niet uitgerekt wordt */
+        object-fit: cover;
     }
 
     .first-article h1 {
@@ -190,7 +180,7 @@
         font-size: var(--title-font-size);
         line-height: var(--title-font-line-height);
         letter-spacing: 0.25px;
-        transition: color 0.3s ease; /* Zachte overgang voor kleurverandering bij hover */
+        transition: color 0.3s ease;
     }
 
     .first-article .title {
@@ -198,17 +188,6 @@
         height: 11em;
         padding: 0.5em;
         overflow: hidden;
-    }
-
-    .first-article .info-text {
-        height: 2em;
-        padding: 0 0.5em;
-        display: flex;
-
-        justify-content: space-between;
-        align-items: center;
-        font-size: 14px;
-        text-transform: uppercase;
     }
 
     .first-article .author {
@@ -226,21 +205,29 @@
         max-width: 1240px;
     }
 
-    /* Hover effect */
     .first-article:hover {
-        transform: scale(1.02);  /* Vergroot het artikel subtiel */
+        transform: scale(1.02);
     }
 
-
-    /* Media Query voor mobiele apparaten */
-    @media only screen and (max-width: 768px) {
+    @media only screen and (max-width: 950px) {
         .first-article {
-            flex-direction: column;  /* Zorgt ervoor dat afbeelding en tekst onder elkaar staan op mobiele schermen */
-            gap: 1em;
+            flex-direction: column;
+        }
+
+        .first-article .everything{
+            padding: 1em;
+        }
+
+        .first-article .excerpt{
+            width: 90%;
+        }
+        
+        .first-article .info-text {
+            width: 90%;
         }
 
         .first-article img {
-            width: 100%;  /* Afbeelding neemt de volledige breedte in op mobiel */
+            width: 100%;
             height: auto;
         }
     }
