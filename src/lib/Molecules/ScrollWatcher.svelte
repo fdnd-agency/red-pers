246c43b94a8ef89e0e06e0119e6221f0f9196c39
RubenErhardt <rubengamebattles@outlook.com>
2025-01-16 19:01:29 +0100

    scrollwatcher creative coding

---
+++ src/lib/Molecules/ScrollWatcher.svelte
@@ -0,0 +1,23 @@
+<div class="scroll-watcher"></div>
+
+<style>
+
+.scroll-watcher {
+      height: 10px;
+      position: fixed;
+      top: 0;
+      z-index: 1000;
+      background-color: var(--accent-color1);
+      transform-origin: left;
+      width: 100%;
+      scale: 0 1;
+      animation: scroll-watcher linear;
+      animation-timeline: scroll();
+      transition: background-image 0.2s ease-in-out; 
+    }
+
+@keyframes scroll-watcher{
+    to { scale: 1 1;}
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,12 @@
     import Article from '$lib/Organism/Article.svelte';
     import CategoryPreview from '$lib/Organism/CategoryPreview.svelte';
     import Nav from '$lib/Organism/Nav.svelte';
+    import ScrollWatcher from '$lib/Molecules/ScrollWatcher.svelte';
 
     export let data;
 </script>
+
+<ScrollWatcher />
 
 <Header alwaysSticky={false}/>
 
