1a348d0598e3f0578c9488012afbdc03672f500f
RubenErhardt <rubengamebattles@outlook.com>
2025-01-21 13:41:48 +0100

    oplossing zoekbalk uit header

--- src/lib/Organism/Header.svelte
+++ src/lib/Organism/Header.svelte
@@ -315,4 +315,26 @@
             --search-background-color: var(--background-color);
         }
     }
+
+    @media screen and (min-width: 320px) and (max-width: 450px){
+        .main-header-inner {
+    display: flex;
+    justify-content: space-between; /* Zorgt voor gelijke ruimte tussen items */
+    align-items: center; /* Centreert items verticaal */
+}
+
+
+        .main-header-inner svg{
+            width: 1.3em;
+            height: 1.3em;
+        }
+
+        .logo-container img{
+            width: 100%;
+            height: auto;
+            max-width: 10em;
+            max-height: 10em;
+        }
+    }
+
 </style>
